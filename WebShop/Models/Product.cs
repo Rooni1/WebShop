9011137b2c3f097521099577d230a6aed3fd0e53
Haroon <haroon_munir@hotmail.com>
2021-10-11 11:44:50 +0200

    Changes in Product class

--- WebShop/Models/Product.cs
+++ WebShop/Models/Product.cs
@@ -9,10 +9,11 @@ namespace WebShop.Models
     {
         public string ProductName { get; set; }
         public string ProductDescription { get; set; }
-        public string ProductSize { get; set; }
+        public float ProductLength { get; set; }
+        public float ProductDimension  { get; set; }
         public int ProductQuantity { get; set; }
         public float ProductPrice { get; set; }
-        public double ArticleNumber { get; set; }
+        public int ArticleNumber { get; set; }
 
     }
 }
