53d568d71a59d1349d816a2873b0b5ae130e7c87
Haroon <haroon_munir@hotmail.com>
2021-10-11 11:23:30 +0200

    Product Class Added

---
+++ WebShop/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductSize { get; set; }
+        public int ProductQuantity { get; set; }
+        public float ProductPrice { get; set; }
+        public double ArticleNumber { get; set; }
+
+    }
+}
