1e0a7019113244cb0b13487cd94e535165bf62fb
Fredrik Ericson <fredrikericson@outlook.com>
2021-10-13 17:16:25 +0200

    Adding ProductDashboard

--- client-app/src/app/layout/App.jsx
+++ client-app/src/app/layout/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 
 import NavBar from './NavBar';
+import ProductDashboard from '../../features/products/dashboard/ProductsDashboard';
 
 function App() {
 
   return (
     <>
       <NavBar />
+      <ProductDashboard />
     </>
   );
 };
