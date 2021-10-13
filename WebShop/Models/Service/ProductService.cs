7069b0f114e3ab9160e97e5d1be5b8dd622cc5a0
Haroon <haroon_munir@hotmail.com>
2021-10-13 16:44:32 +0200

    Added backed code

--- WebShop/Models/Service/ProductService.cs
+++ WebShop/Models/Service/ProductService.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebShop.Models.Entities;
+using WebShop.Models.Repo;
 using WebShop.ViewModels;
 
 namespace WebShop.Models.Service
 {
+   
     public class ProductService : IProductService
     {
+        private readonly ProductRepo _productRepo;
+        public ProductService(ProductRepo productRepo)
+        {
+            productRepo = _productRepo;
+
+        }
         public void Add(CreateProductViewModel product)
         {
-            throw new NotImplementedException();
+            _productRepo.Create(product);
         }
 
-        public List<ProductViewModel> All()
+        public ProductViewModel All()
         {
-            throw new NotImplementedException();
+            ProductViewModel productVVM = new ProductViewModel { products = _productRepo.Read() };
+            return productVVM;
         }
 
         public void Edit(int id, UpdateProductViewModel product)
@@ -24,14 +33,15 @@ namespace WebShop.Models.Service
             throw new NotImplementedException();
         }
 
-        public ProductViewModel FindBy(int id)
+        public Product FindBy(int id)
         {
-            throw new NotImplementedException();
+            return _productRepo.Read(id);
         }
 
         public void Remove(int id)
         {
-            throw new NotImplementedException();
+            Product productToRemove = _productRepo.Read(id);
+            _productRepo.Delete(productToRemove);
         }
     }
 }
