fc40841e3912378f7ec371dc41386656f2ca1f69
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-10-20 14:57:07 +0200

    hämtar in ändringar från master

--- WebShop/Controllers/ProductController.cs
+++ WebShop/Controllers/ProductController.cs
@@ -6,59 +6,59 @@ using WebShop.ViewModels;
 
 namespace WebShop.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private readonly IProductService _productService;
+	private readonly IProductService _productService;
 
-        public ProductController(IProductService productService)
-        {
-            _productService = productService;
-        }
+	public ProductController(IProductService productService)
+	{
+	    _productService = productService;
+	}
 
 
-        [HttpGet]
-        [Route("GetAll")]
-        public ActionResult<List<ProductViewModel>> GetAll()
+	[HttpGet]
+	[Route("GetAll")]
+	public ActionResult<List<ProductViewModel>> GetAll()
 
-        {
-            List<ProductViewModel> products = _productService.All();
+	{
+	    List<ProductViewModel> products = _productService.All();
 
-            return Ok(products);
+	    return Ok(products);
 
-        }
+	}
 
-        [HttpGet("{id}")]
-        public ActionResult<ProductViewModel> Get(int id)
-        {
-            ProductViewModel product =  _productService.FindBy(id);
+	[HttpGet("{id}")]
+	public ActionResult<ProductViewModel> Get(int id)
+	{
+	    ProductViewModel product =  _productService.FindBy(id);
 
-            return Ok(product);
-        }
+	    return Ok(product);
+	}
 
-        [HttpPost]
-        [Route("Create")]
-        public ActionResult Create([FromBody] CreateProductViewModel product)
-        {
-            _productService.Add(product);
-            return NoContent();
-        }
+	[HttpPost]
+	[Route("Create")]
+	public ActionResult Create([FromBody] CreateProductViewModel product)
+	{
+	    _productService.Add(product);
+	    return NoContent();
+	}
 
-        [HttpPut("{id}")]
-        [Route("Update")]
-        public IActionResult Update(int id, UpdateProductViewModel product)
-        {
-            _productService.Edit(id,product);
-            return NoContent();
-        }
+	[HttpPut("{id}")]
+	[Route("Update")]
+	public IActionResult Update(int id, UpdateProductViewModel product)
+	{
+	    _productService.Edit(id,product);
+	    return NoContent();
+	}
 
-        [HttpDelete("{id}")]
-        [Route("Delete")]
-        public ActionResult Delete(int id)
-        {
-            _productService.Remove(id);
-            return NoContent();
-        }
+	[HttpDelete("{id}")]
+	[Route("Delete")]
+	public ActionResult Delete(int id)
+	{
+	    _productService.Remove(id);
+	    return NoContent();
+	}
     }
 }
--- client-app/src/app/layout/App.jsx
+++ client-app/src/app/layout/App.jsx
@@ -6,6 +6,7 @@ import './App.css';
 import NavBar from './NavBar';
 import ProductDashboard from '../../features/products/dashboard/ProductsDashboard';
 import ProductDetails from '../../features/products/details/ProductDetails';
+import Basket from '../../features/order/Basket';
 
 
 function App() {
@@ -13,17 +14,18 @@ function App() {
   return (
     <>
       <Route
-        path={'/(.+)'}
-        render={() => (
-          <>
-            <NavBar />
-            <Switch>
-              <Route exact path='/shop' component={ProductDashboard} />
-              <Route path='/shop/:id' component={ProductDetails} />
-            </Switch>
-          </>
-        
-        )}
+	path={'/(.+)'}
+	render={() => (
+	  <>
+	    <NavBar />
+	    <Switch>
+	      <Route exact path='/shop' component={ProductDashboard} />
+	      <Route path='/shop/:id' component={ProductDetails} />
+		<Route path='/basket' component={Basket} />
+	    </Switch>
+	  </>
+
+	)}
       />
     </>
   );
