822d0d1a18c23bdb940f3fa957062fae282f9842
Fredrik Ericson <fredrikericson@outlook.com>
2021-10-20 10:11:40 +0200

    Added handleClick shopping cart

--- client-app/src/app/layout/NavBar.jsx
+++ client-app/src/app/layout/NavBar.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 import { Row, Col, Navbar, Nav } from 'react-bootstrap';
+import { history } from '../..';
+
+function handleClick() {
+  history.push('/basket/');
+};
 
 export default function NavBar() {
   return (
@@ -17,8 +22,8 @@ export default function NavBar() {
       </div>
       <div className="d-flex justify-content-center">  
         <a className="position-relative me-2 ">
-          <i className="fa fa-shopping-cart fa-2x text-dark"></i>
-          <div className="cart-no mt-2">5</div>
+          <i className="fa fa-shopping-cart fa-2x text-dark" onClick={handleClick}></i>
+          <div className="mt-2 cart-no">5</div>
         </a>
         <a className="btn btn-outline-primary">Login</a>
         <a className="ms-2 btn btn-outline-secondary">Sign up</a>
