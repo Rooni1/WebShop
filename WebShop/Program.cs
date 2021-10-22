dca992345adc31f626f729b510d85ca731b64235
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-10-22 13:53:27 +0200

    formattering och kommentarer i Program.cs och Startup.cs

--- WebShop/Program.cs
+++ WebShop/Program.cs
@@ -16,35 +16,35 @@ namespace WebShop
 {
     public class Program
     {
-        public static async Task Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
+	public static async Task Main(string[] args)
+	{
+	    var host = CreateHostBuilder(args).Build();
 
-            using var scope = host.Services.CreateScope();
-            var services = scope.ServiceProvider;
-            //var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-            try
-            {
+	    using var scope = host.Services.CreateScope();
+	    var services = scope.ServiceProvider;
+	    //var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+	    try
+	    {
 
-                var userManager = services.GetRequiredService<UserManager<AppUser>>();
-                var identityContext = services.GetRequiredService<AppIdentityDbContext>();
-                await identityContext.Database.MigrateAsync();
-                await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
-            }
-            catch (Exception ex)
-            {
-                //var logger = loggerFactory.CreateLogger<Program>();
-                //logger.LogError(ex, "An error occurred during migration");
-            }
+		var userManager = services.GetRequiredService<UserManager<AppUser>>();
+		var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+		await identityContext.Database.MigrateAsync();
+		await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
+	    }
+	    catch (Exception ex)
+	    {
+		//var logger = loggerFactory.CreateLogger<Program>();
+		//logger.LogError(ex, "An error occurred during migration");
+	    }
 
-            host.Run();
-        }
+	    host.Run();
+	}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+	public static IHostBuilder CreateHostBuilder(string[] args) =>
+	    Host.CreateDefaultBuilder(args)
+	    .ConfigureWebHostDefaults(webBuilder =>
+	    {
+		webBuilder.UseStartup<Startup>();
+	    });
     }
 }
