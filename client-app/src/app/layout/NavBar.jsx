ab74a69701cf8426dc82372f6ac454349ed26e71
Fredrik Ericson <fredrikericson@outlook.com>
2021-10-25 15:26:00 +0200

    New headline

--- client-app/src/app/layout/NavBar.jsx
+++ client-app/src/app/layout/NavBar.jsx
@@ -26,7 +26,7 @@ export default observer(function NavBar() {
   return (
     <div className="p-3 mb-3 bg-white shadow-sm d-flex flex-column flex-md-row align-items-center justify-content-between px-md-4 border-bottom">
       <div className="d-flex flex-column align-items-center">
-        <h5 className="my-0 mr-md-auto font-weight-normal">E-commerce</h5>
+        <h5 className="my-0 mr-md-auto font-weight-normal">WebShop</h5>
       </div>
       <div className="d-flex flex-column flex-md-row align-items-center">
         <nav className="my-2 my-md-0 mr-md-3 text-uppercase" style={{fontSize: "larger;"}}>
