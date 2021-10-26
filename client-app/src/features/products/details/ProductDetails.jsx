33cb12c9e8774a8e633b030e9e941d9f4176dc1f
Fredrik Ericson <fredrikericson@outlook.com>
2021-10-26 09:05:55 +0200

    Changed layout

--- client-app/src/features/products/details/ProductDetails.jsx
+++ client-app/src/features/products/details/ProductDetails.jsx
@@ -19,24 +19,28 @@ export default observer(function ProductDetails() {
     <>
       <div className="row">
         <div className="col-4 ms-4">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Screw_for_wood.JPG/256px-Screw_for_wood.JPG" alt="" className="img-fluid"></img>
-        </div>
-        <div className="col-4">
-          <h3>{product.name}</h3>
-          <p>{product.productPrice}</p>
-          <div className="d-flex justify-content-start align-items-center">
-            <i className="fa fa-minus-circle text-warning me-2" style={{cursor: "pointer;", fontSize: "2em"}}></i>
-            <span className="font-weight-bold" style={{fontSize: "1.5em"}}>2</span>
-            <i className="mx-2 fa fa-plus-circle text-warning" style={{cursor: "pointer;", fontSize: "2em"}}></i>
-            <button className="btn btn-outline-secondary btn-lg ms-4">Add to cart</button>
+          <div className="row">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Screw_for_wood.JPG/256px-Screw_for_wood.JPG" alt="" className="img-fluid"></img>
           </div>
-        </div>
-        <div className="mt-5 row ms-2">
-            <div className="ml-3 col-12">
-              <h4>Description</h4>
-              <p>{product.productDescription}</p>
+          <div className="row">
+            <div className="col-4">
+              <button className="btn btn-outline-secondary btn-lg">Till listan</button>
             </div>
           </div>
+        </div>
+        <div className="col-4">
+          <div className="d-flex justify-content-start align-items-center">
+            <div className="ml-3 col-12">
+              <h4>Produktnamn</h4>
+              <p>{product.name}</p>
+              <h4>Produktbeskrivning</h4>
+              <p>{product.productDescription}</p>
+              <h4>Pris</h4>
+              <p>{product.productPrice}</p>
+              <button className="btn btn-outline-secondary btn-lg">Lägg i varukorg</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
