5f7ca7fb2cfa362b7d8aa98cebfd1200cf82b966
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-10-31 18:21:11 +0100

    formattering och timestamp i filhuvud

--- client-app/src/features/order/OrderTotals.jsx
+++ client-app/src/features/order/OrderTotals.jsx
@@ -12,32 +12,32 @@ export default observer(function OrderTotal() {
   const {total} = orderStore;
 
   const [totalSum, setTotalSum] = useState(0);
-  
+
   useEffect(() => {
     setTotalSum(total);
   }, [total]);
-  
+
   return (
     <>
-      <div className="px-4 text-uppercase" 
-        style={{padding: "1.20em", backgroundColor: "lightgray", fontWeight: "bold"}}>
-          Order Summary
+      <div className="px-4 text-uppercase"
+	style={{padding: "1.20em", backgroundColor: "lightgray", fontWeight: "bold"}}>
+	  Order Summary
       </div>
       <div className="p-4">
-          <ul className="mb-4 list-unstyled">
-              <li className="py-3 d-flex justify-content-between border-bottom">
-                  <strong className="text-muted">Order subtotal</strong>
-                  <strong>{totalSum}</strong>
-              </li>
-              <li className="py-3 d-flex justify-content-between border-bottom">
-                  <strong className="text-muted">Shipping and handling</strong>
-                  <strong>{totalSum ? 10 : 0}</strong>
-              </li>
-              <li className="py-3 d-flex justify-content-between border-bottom">
-                  <strong className="text-muted">Total</strong>
-                  <strong>{totalSum ? totalSum + 10 : 0}</strong>
-              </li>
-          </ul>
+	  <ul className="mb-4 list-unstyled">
+	      <li className="py-3 d-flex justify-content-between border-bottom">
+		  <strong className="text-muted">Order subtotal</strong>
+		  <strong>{totalSum}</strong>
+	      </li>
+	      <li className="py-3 d-flex justify-content-between border-bottom">
+		  <strong className="text-muted">Shipping and handling</strong>
+		  <strong>{totalSum ? 10 : 0}</strong>
+	      </li>
+	      <li className="py-3 d-flex justify-content-between border-bottom">
+		  <strong className="text-muted">Total</strong>
+		  <strong>{totalSum ? totalSum + 10 : 0}</strong>
+	      </li>
+	  </ul>
       </div>
     </>
   );
