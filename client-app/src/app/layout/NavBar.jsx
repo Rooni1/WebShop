2713ae5cd05714f832a614088084e8bc014549f6
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-10-31 18:13:26 +0100

    formattering

--- client-app/src/app/layout/NavBar.jsx
+++ client-app/src/app/layout/NavBar.jsx
@@ -1,3 +1,7 @@
+//
+// Time-stamp: <2021-10-31 18:08:37 stefan>
+//
+
 import React, { useState, useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Link } from 'react-router-dom';
@@ -26,7 +30,7 @@ export default observer(function NavBar() {
   return (
     <div className="p-3 mb-3 bg-white shadow-sm d-flex flex-column flex-md-row align-items-center justify-content-between px-md-4 border-bottom">
       <div className="d-flex flex-column align-items-center">
-        <h5 className="my-0 mr-md-auto font-weight-normal">WebShop</h5>
+	<h5 className="my-0 mr-md-auto font-weight-normal">WebShop</h5>
       </div>
       <div className="d-flex flex-column flex-md-row align-items-center">
       </div>
